31781a8306782deb205437a8b92a37d7c05f7278
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2022-03-23 13:51:44 +0300

    Map the API data onto some JSX elements

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import WelcomePage from './components/welcome-page/WelcomePage';
+import questions from './components/api/api';
 import './App.css';
 
 function App() {
+
   return (
     <div>
       <WelcomePage />
+      { questions[0].map((question, index) => (
+        <p key={index}>{question.question}</p>
+      )) }
     </div>
   );
 }
