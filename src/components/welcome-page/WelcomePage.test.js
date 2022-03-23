ce71a22ab02ba4c4cb409f0e6e55bc50d55229cf
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2022-03-23 10:51:20 +0300

    Fix eslint errors

--- src/components/welcome-page/WelcomePage.test.js
+++ src/components/welcome-page/WelcomePage.test.js
@@ -1,9 +1,9 @@
 import '@testing-library/jest-dom';
-import {render, screen} from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import WelcomePage from './WelcomePage';
 
 test('renders correct welcome page', () => {
   render(<WelcomePage />);
   const title = screen.getByText(/Quizzical/);
   expect(title).toBeInTheDocument();
-})
+});
