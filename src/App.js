44a67ec557dea87d68c5a245f8d976c204159e48
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2022-03-23 13:56:56 +0300

    Fix stylelint issues

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
+/* eslint-disable  react/no-array-index-key */
 import WelcomePage from './components/welcome-page/WelcomePage';
 import questions from './components/api/api';
 import './App.css';
 
 function App() {
-
   return (
     <div>
       <WelcomePage />
--- src/components/api/api.js
+++ src/components/api/api.js
@@ -1,6 +1,6 @@
 const questions = [];
 fetch('https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=multiple')
   .then((res) => res.json())
-  .then((data) => questions.push(data.results))
+  .then((data) => questions.push(data.results));
 
 export default questions;
