09d3a3eb41108d3e5738f6edb830ab2784e5cf94
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2022-03-23 10:25:25 +0300

    Fix Eslint errors

--- src/components/welcome-page/WelcomePage.js
+++ src/components/welcome-page/WelcomePage.js
@@ -1,22 +1,22 @@
-import React from 'react'
-import './welcomePage.css'
-import blobTop from '../../images/blobs.png'
-import blobBottom from '../../images/blobs1.png'
+import React from 'react';
+import './welcomePage.css';
+import blobTop from '../../images/blobs.png';
+import blobBottom from '../../images/blobs1.png';
 
 function WelcomePage() {
   return (
     <div>
-      <img src={ blobTop } className="blob-top" />
+      <img src={blobTop} className="blob-top" alt="top-blob" />
       <div className="welcome-page">
         <div>
           <h1>Quizzical</h1>
           <p>Test your knowledge of common quizzes</p>
-          <button className="start-game-button">Start Game</button>
+          <button type="button" className="start-game-button">Start Game</button>
         </div>
       </div>
-      <img src={ blobBottom } className="blob-bottom"/>
+      <img src={blobBottom} className="blob-bottom" alt="bottom-blob" />
     </div>
-  )
+  );
 }
 
-export default WelcomePage
+export default WelcomePage;
