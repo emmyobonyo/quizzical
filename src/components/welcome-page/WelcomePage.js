60a6f7d2ce83f4f45035ca4697bc266541c321fb
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2022-03-23 09:59:14 +0300

    Add blob image to upper-right corner

--- src/components/welcome-page/WelcomePage.js
+++ src/components/welcome-page/WelcomePage.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import './welcomePage.css'
+import blobs from '../../images/blobs.png'
 
 function WelcomePage() {
   return (
-    <div className="welcome-page">
-      <div>
-        <h1>Quizzical</h1>
-        <p>Test your knowledge of common quizzes</p>
-        <button className="start-game-button">Start Game</button>
+    <div>
+      <img src={ blobs } className="blobs"/>
+      <div className="welcome-page">
+        <div>
+          <h1>Quizzical</h1>
+          <p>Test your knowledge of common quizzes</p>
+          <button className="start-game-button">Start Game</button>
+        </div>
       </div>
     </div>
   )
