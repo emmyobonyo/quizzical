795e7c7224b9c9758a3ed56ff890e5ab75a7a308
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2022-03-25 12:44:13 +0300

    Revert back to default encoding API

--- src/components/api/api.js
+++ src/components/api/api.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-useless-escape */
 async function getQuestions() {
-  const response = await fetch('https://opentdb.com/api.php\?amount\=5\&category\=9\&difficulty\=easy\&type\=multiple');
+  const response = await fetch('https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=multiple');
   const data = await response.json();
   return data;
 }
