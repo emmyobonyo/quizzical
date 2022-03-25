ccd91b9292a4b7780901a4e9170893db35b0aaa1
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2022-03-25 22:57:03 +0300

    Change color on click

--- src/components/questionnaire/Questionaire.js
+++ src/components/questionnaire/Questionaire.js
@@ -9,21 +9,6 @@ import BlogImages from '../blob-images/BlogImages';
 import './Questionaire.css';
 
 function Questionaire({ questions }) {
-  // const answersArray = [];
-  // answersArray.push({
-  //   id: correctAnswer,
-  //   correctAnswer: true,
-  //   title: correctAnswer,
-  //   clicked: false,
-  // });
-  // incorrectAnswers.map((incorrectAnswer) => (
-  //   answersArray.push({
-  //     id: incorrectAnswer,
-  //     correctAnswer: false,
-  //     title: incorrectAnswer,
-  //     clicked: false,
-  //   })
-  // ));
   const answerArray = [];
   questions.map((question, index) => (
     answerArray.push({
@@ -45,22 +30,22 @@ function Questionaire({ questions }) {
     }
   });
 
-  const [answers] = useState(answerArray);
+  const [answers, setAnswers] = useState(answerArray);
   console.log(answers);
 
-  // function clickAnswer(id) {
-  //   return setAnswers((answer) => answer.map((answer) => (
-  //     answer.id === id || answer.clicked === true ? { ...answer, clicked: !answer.clicked }
-  //       : answer
-  //   )));
-  // }
+  function clickAnswer(id) {
+    return setAnswers((answer) => answer.map((answer) => (
+      answer.id === id || answer.clicked === true ? { ...answer, clicked: !answer.clicked }
+        : answer
+    )));
+  }
 
   const quiz = questions.map((question, index) => (
     <div className="quiz" key={question.question}>
       <h1 className="quiz-heading">{question.question}</h1>
       <div className="quiz-answers">
         { answers.map((answer) => (
-          answer.question === index ? <div key={answer.id} className={question.clicked ? 'className' : 'answers'} /* onClick={() => clickAnswer(answersArray.id)} */ aria-hidden="true">{answer.title}</div> : ''
+          answer.question === index ? <div key={answer.id} className={answer.clicked ? 'clicked' : 'answers'} onClick={() => clickAnswer(answer.id)} aria-hidden="true">{answer.title}</div> : ''
         )) }
       </div>
     </div>
